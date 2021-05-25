0149863d7b096365e18727c7f5649efed8fdc95c
hongtaat <42405931+hongtaat@users.noreply.github.com>
2021-05-26 01:05:28 +0800

    Update and rename install.sh to customize.sh

---
+++ customize.sh
@@ -0,0 +1,8 @@
+# Set what you want to display when installing your module
+
+print_modname() {
+  ui_print "*****************************************"
+  ui_print "           MIUI 10 SOUNDS PACK    "
+  ui_print "   Extracted and Packed by 4PERTURE   "
+  ui_print "******************************************"
+}
